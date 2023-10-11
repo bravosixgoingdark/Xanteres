e4805c47a419e37dcf0128f3378da5c404c854e8
Pric3 <minhnhatbg05@gmail.com>
2023-10-11 12:49:11 +0700

    Add gtts and voice recognition(WIP)

--- dockerfile
+++ dockerfile
@@ -1,6 +1,5 @@
 FROM ubuntu:22.04 
 WORKDIR /app
 COPY . .
-RUN apt-get update && apt-get install -y python3 python3-pip && pip3 install -r requirements.txt
+RUN apt-get update && apt-get install -y python3.12 mpg123 python3-pip libasound-dev libportaudio2 libportaudiocpp0 portaudio19-dev && pip3 install -r requirements.txt
 CMD ["python3", "main.py"]
-```
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# todo: write main file
+from gtts import gTTS
+import os 
+import speech_recognition as sr
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    filename = 'voice.mp3'
+    tts.save(filename)
+    os.system('mpg123 ' + filename)
+    os.system('rm' + filename)
+
+def speak_recognition():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+    try:
+        print("RECOG:" + r.recognize_google(audio))
+        speak(r.recognize_google(audio))
+    except:
+        print("Sorry could not recognize what you said")
+        exit(0)
+
+if __name__ == '__main__':
+    speak_recognition()
